7374eda63890992ba48193f290ce6e74918a1407
Maira Flores <73151482+MairaRomina@users.noreply.github.com>
2022-09-01 12:06:29 -0300

    Add files via upload

---
+++ Ej1 encadenado/main.py
@@ -0,0 +1,11 @@
+from ej1 import Pila
+
+if __name__ == '__main__':
+    lista = Pila()
+    lista.insertar(1)
+    lista.insertar(2)
+    lista.insertar(3)
+    lista.insertar(4)
+    lista.mostrar()
+    lista.mostrar()
+
